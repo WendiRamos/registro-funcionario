ff5551170a3e61463ec72f4825c78c8a11f7d160
Wendi Ramos <wendiramos12@gmail.com>
2021-05-14 08:39:55 -0300

    Create HourContract.cs

---
+++ Entidades/HourContract.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace Registro_Funcionario.Entities
+{
+    class HourContract
+    {
+
+        public DateTime Date { get; set; }
+        public double ValuePerHour { get; set; }
+        public int Hours { get; set; }
+
+        public HourContract()
+        {
+        }
+
+        public HourContract(DateTime date, double valuePerHour, int hours)
+        {
+            Date = date;
+            ValuePerHour = valuePerHour;
+            Hours = hours;
+        }
+
+        public double TotalValue()
+        {
+            return Hours * ValuePerHour;
+        }
+    }
+}
