7561a8ead23a67ccd022526e152511d37781ba66
Wendi Ramos <wendiramos12@gmail.com>
2021-05-14 08:39:33 -0300

    Create Department.cs

---
+++ Entidades/Department.cs
@@ -0,0 +1,17 @@
+namespace Registro_Funcionario.Entities
+{
+    class Department
+    {
+
+        public string Name { get; set; }
+
+        public Department()
+        {
+        }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
